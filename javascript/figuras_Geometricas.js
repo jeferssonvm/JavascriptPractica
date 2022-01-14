514802b5eaf6bfd70a7eca631e618ef6b69d2ff6
Jefersson Velasquez <jeferssonveme.97@gmail.com>
2022-01-13 19:29:55 -0500

    iniciando javascript

---
+++ javascript/figuras_Geometricas.js
@@ -0,0 +1,12 @@
+function perimetroCuadrado(lado){
+    return 4*lado;
+}
+function areaCuadrado(lado){
+    return Math.pow(lado,2);
+}
+function perimetroTriangulo(lado1,lado2,lado3){
+    return lado1 + lado2 + lado3;
+}
+function areaTriangulo(base, altura){
+    return  (base*altura)/2
+}
