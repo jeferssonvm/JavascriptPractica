5234a2009ea448bd99b3c74e831fdf9105dccc70
Jefersson Velasquez <jeferssonveme.97@gmail.com>
2022-01-15 19:49:27 -0500

    finalizacion del las cartas creacion del condicional if para el triangulo isoceles

--- javascript/figuras_Geometricas.js
+++ javascript/figuras_Geometricas.js
@@ -1,75 +1,131 @@
 
 /*Cuadrado*/
 function perimetroCuadrado(lado){
-    return 4*lado;
+    return (4*lado).toFixed(2);
 }
 function areaCuadrado(lado){
-    return Math.pow(lado,2);
+    return (Math.pow(lado,2)).toFixed(2);
 }
-/*Triangulo*/
-function perimetroTriangulo(lado1,lado2,lado3){
-    return lado1 + lado2 + lado3;
-}
-function areaTriangulo(base, altura){
-    return  (base*altura)/2;
-}
-/*Rectangulo*/
-function perimetroRectangulo(baseRectangulo,alturaRectangulo){
-    return (2*baseRectangulo)+(2*alturaRectangulo);
-}
-function areaRectangulo(baseRectangulo,alturaRectangulo){
-    return (baseRectangulo*alturaRectangulo);
-}
+
 /*rombo*/
 function perimetroRombo(ladoRombo){
-    return ladoRombo*4;
+    return (ladoRombo*4).toFixed(2);
 }
 function areaRombo(diagonalMenorRombo,diagonalMayorRombo){
-    return (diagonalMayorRombo*diagonalMenorRombo)/2;
-}
-/*romboide*/
-function perimetroRomboide(baseRomboide,alturaRomboide){
-    return (2*baseRomboide)+(2*alturaRomboide);
-}
-function areaRomboide(baseRomboide,alturaRomboide){
-    return (baseRomboide*alturaRomboide);
-}
-/*trapecio*/
-function perimetroTrapecio(lado1,lado2,lado3,lado4){
-    return (lado1+lado2+lado3+lado4);
-}
-function areaTrapecio(baseMenor,baseMayo,altura){
-    return ((altura(baseMayo+baseMenor))/2);
-}
-/*pentagono*/
-function perimetroPentagono(base){
-    return (5*base);
-}
-function areaPentagono(apotema,base){
-    return (((5*base)*apotema)/2);
-}
-/*Hexagono*/
-function perimetroHexagono(base){
-    return (6*base);
-}
-function areaHexagono(base,apotema){
-    return (((6*base)*apotema)/2);
+    return ((diagonalMayorRombo*diagonalMenorRombo)/2).toFixed(2);
 }
 /*Circulo*/
 function perimetroCirculo(diametro){
-    return (diametro*(Math.PI));
+    return (diametro*(Math.PI)).toFixed(2);
 }
 function areaCirculo(radio){
-    return ((Math.PI)*Math.pow(radio,2));
+    return ((Math.PI)*Math.pow(radio,2)).toFixed(2);
 }
 
+
+
+
+
+/*Triangulo Equilatero
+function perimetroTriangulo(lado1,lado2,lado3){
+    return (lado1 + lado2 + lado3).toFixed(2);
+}
+function areaTriangulo(base, altura){
+    return  ((base*altura)/2).toFixed(2);
+}*/
+/*romboide
+function perimetroRomboide(baseRomboide,alturaRomboide){
+    return (2*baseRomboide)+(2*alturaRomboide).toFixed(2);
+}
+function areaRomboide(baseRomboide,alturaRomboide){
+    return (baseRomboide*alturaRomboide).toFixed(2);
+}*/
+/*trapecio
+function perimetroTrapecio(lado1,lado2,lado3,lado4){
+    return (lado1+lado2+lado3+lado4).toFixed(2);
+}
+function areaTrapecio(baseMenor,baseMayo,altura){
+    return ((altura(baseMayo+baseMenor))/2).toFixed(2);
+}*/
+/*pentagono
+function perimetroPentagono(base){
+    return (5*base).toFixed(2);
+}
+function areaPentagono(apotema,base){
+    return (((5*base)*apotema)/2).toFixed(2);
+}*/
+/*Hexagono
+function perimetroHexagono(base){
+    return (6*base).toFixed(2);
+}
+function areaHexagono(base,apotema){
+    return (((6*base)*apotema)/2).toFixed(2);
+}*/
+
+/*triangulo isoceles*/
+
+    function trianguloIsoceles(lado1, lado2,lado3){
+    
+        if (lado1==lado2 || lado1 == lado3 || lado2 == lado3 ){
+            if (lado1==lado2 && lado1 == lado3){
+                alert("el triangulo es equilatero")
+            }else{
+               if(lado1==lado2 ){
+            
+               }
+               if(lado1==lado3){
+
+               } 
+               if(lado2==lado3){
+                   
+            } 
+            }
+        }else{
+            alert(" el triangulo es escaleno ")
+        }
+        
+    }
+
+/*entrada html*/
 function calcularPerimetroCuadrado(){
-    alert("entre");
     const input = document.getElementById("InputCuadrado");
     const value = input.value;
-
     const perimetro = perimetroCuadrado(value);
-    document.getElementById("CuadradoSalida").innerHTML = perimetro;
-    alert(perimetro);
-    
+    document.getElementById("PerimetroCuadradoSalida").innerHTML = perimetro;
+}
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    document.getElementById("AreaCuadradoSalida").innerHTML= area;
+}
+
+function calcularAreaCirculo(){
+    const inputRadio = document.getElementById("RadioCirculo")
+    const valueRadio = inputRadio.value;
+    const area = areaCirculo(valueRadio);
+    document.getElementById("AreaCirculoSalida").innerHTML = area;
+}
+
+function  calcularPerimetroCirculo(){
+    const inputDiametro = document.getElementById("DiametroCirculo");
+    const valueDiametro =inputDiametro.value;
+    const perimetro = perimetroCirculo(valueDiametro);
+    document.getElementById("PerimetroCirculoSalida").innerHTML= perimetro;
+}
+
+function calcularPerimetroRombo() {
+    const inputLado = document.getElementById("ladoRombo");
+    const valueLado = inputLado.value;
+    const perimetro = perimetroRombo(valueLado);
+    document.getElementById("PerimetroRomboSalida").innerHTML = perimetro;
+}
+function calcularAreaRombo(){
+    const inputMayor = document.getElementById("DiagonalMayor");
+    const valueMayor = inputMayor.value;
+    const inputMenor = document.getElementById("DiagonalMenor");
+    const valueMenor = inputMenor.value;
+    const area = areaRombo( valueMenor,valueMayor );
+    alert(area);
+    document.getElementById("AreaRomboSalida").innerHTML = area;
 }
