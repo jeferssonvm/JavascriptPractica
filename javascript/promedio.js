d2e087ca07b2a2c0ca23c9f04bb7d4c62500d0c5
Jefersson Velasquez <jeferssonveme.97@gmail.com>
2022-01-22 16:31:56 -0500

    se creo la estructura del promedio moda median y codigo de promedio

---
+++ javascript/promedio.js
@@ -0,0 +1,16 @@
+
+// const sumDateList =listElement.reduce(function(accumulatedValue =0,newValue){
+//     return accumulatedValue +newValue;
+// })
+const listElement =[2,5,1,5,7,3,6,9,5,0,2,4,8,7,9,2,1,8,6,7,20,54,63,20,42];
+
+
+function findAverage(listItem){
+    const sumDateList =listItem.reduce(function(accumulatedValue =0,newValue){
+        return accumulatedValue +newValue;
+    })
+    const resultAverage = sumDateList/listItem.length;    
+    return resultAverage;
+}
+
+let list = findAverage(listElement);
